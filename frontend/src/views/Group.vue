1c8a204b7f7f20fa5b9017720b6fcb9eae7f076c
ShomuraYuki <yski0616@gmail.com>
2020-03-06 11:44:03 +0900

    no message

---
+++ frontend/src/views/Group.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="Group">
+    <h1>This is a Group page</h1>
+    <p>{{ todos }}</p>
+    <DAD />
+  </div>
+</template>
+
+<script>
+  import DAD from "@/components/DAD.vue";
+  import axios from "axios";
+
+  export default {
+    name: "Group",
+    components: {
+      DAD
+    },
+    data() {
+      return {
+        url: "http://127.0.0.1:8000/api/",
+        todos: ""
+      }
+    },
+    mounted() {
+    axios.get(this.url + "group/")
+    .then(response => {
+      this.todos = response
+    })
+  }
+  };
+</script>
